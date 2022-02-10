164456772a1323e1ee7ce469d5308167a28184c9
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-02-10 20:58:23 +0800

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 class App {
+  textures = {}
   versions = [
     '1.13',
     '1.14',
@@ -10,10 +11,77 @@ class App {
 
   constructor() {
     this.params = new URLSearchParams(window.location.search)
-    this.interval = this.params.get('interval') || 120
+    this.interval = this.getInterval()
+    this.control = this.getControl()
+
+    this.initOptions()
+    document.body.style.backgroundImage = `url(${this.path}background${this.ext})`
+
+    this.panorama = new Panorama({
+      cube: [
+        this.path + 'panorama_1' + this.ext,
+        this.path + 'panorama_3' + this.ext,
+        this.path + 'panorama_4' + this.ext,
+        this.path + 'panorama_5' + this.ext,
+        this.path + 'panorama_0' + this.ext,
+        this.path + 'panorama_2' + this.ext
+      ],
+      options: {
+        controlBar: this.control,
+        autoRotate: true,
+        cameraFov: 85,
+        autoRotateSpeed: 0.3,
+        autoRotateActivationDuration: 0,
+        initialLookAt: new THREE.Vector3(0, 0, 2)
+      }
+    })
   }
 
-  init() {
+  getInterval() {
+    const defaultInterval = 15
+    const interval = this.params.get('interval')
+
+    return interval === null ?
+      defaultInterval :
+      isNaN(interval) ?
+        defaultInterval :
+        Number(interval)
+  }
+
+  getControl() {
+    const defaultControl = false
+    const control = this.params.get('control')
+
+    return control === null ?
+      defaultControl :
+      control == 'true'
+  }
+
+  loadTextures(version) {
+    if (!this.textures[version]) {
+      const ext = '.png'
+      const path = `src/images/${version}/`
+      const texture = new PANOLENS.CubeTextureLoader.load(
+        [
+          path + 'panorama_1' + ext,
+          path + 'panorama_3' + ext,
+          path + 'panorama_4' + ext,
+          path + 'panorama_5' + ext,
+          path + 'panorama_0' + ext,
+          path + 'panorama_2' + ext
+        ],
+        () => {
+          this.panorama.updateTextures(texture)
+        }
+      )
+
+      this.textures[version] = texture
+    } else {
+      this.panorama.updateTextures(this.textures[version])
+    }
+  }
+
+  initOptions() {
     if (this.panorama) {
       const currentVersionIdx = Object.values(this.versions)
         .findIndex((version) => this.selectedVersion === version)
@@ -25,55 +93,34 @@ class App {
       }
     } else {
       const query = this.params.get('version')
-      const version = this.versions.includes(query) ? query : this.versions[this.versions.length - 1]
+      const version = this.versions.includes(query) ?
+        query :
+        this.versions[this.versions.length - 1]
+
       this.url = new URL(location.href)
       this.selectedVersion = version
     }
 
     this.url.searchParams.set('version', this.selectedVersion)
+    this.url.searchParams.set('interval', this.interval)
+    this.url.searchParams.set('control', this.control)
     history.pushState(null, '', this.url)
 
     this.path = `src/images/${this.selectedVersion}/`
     this.ext = '.png'
-
-    if (!this.panorama) {
-      document.body.style.backgroundImage = `url(${this.path}background${this.ext})`
-    }
   }
 
-  render() {
-    this.panorama = new Panorama({
-      cube: [
-        this.path + 'panorama_1' + this.ext,
-        this.path + 'panorama_3' + this.ext,
-        this.path + 'panorama_4' + this.ext,
-        this.path + 'panorama_5' + this.ext,
-        this.path + 'panorama_0' + this.ext,
-        this.path + 'panorama_2' + this.ext
-      ],
-      options: {
-        controlBar: false,
-        autoRotate: true,
-        cameraFov: 85,
-        autoRotateSpeed: 0.3,
-        autoRotateActivationDuration: 0,
-        initialLookAt: new THREE.Vector3(0, 0, 2)
-      }
-    })
-
+  start() {
     this.panorama.start()
-  }
 
-  run() {
-    if (this.panorama) {
-      this.panorama.destroy()
+    if (this.interval > 0) {
+      setInterval(() => {
+        this.initOptions()
+        this.loadTextures(this.selectedVersion)
+      }, 1000 * this.interval)
     }
-
-    this.init()
-    this.render()
-    setTimeout(() => this.run(), 1000 * this.interval)
   }
 }
 
 const app = new App()
-app.run()
+app.start()
--- src/js/panorama.js
+++ src/js/panorama.js
@@ -1,71 +1,17 @@
 class Panorama {
   constructor({ cube, options }) {
-    this.renderProgress()
-
-    this.filters = [
-      'none',
-      'blur(4px)',
-      'brightness(1.5)',
-      'contrast(1.5)',
-      'grayscale(1)',
-      'hue-rotate(180deg)',
-      'invert(1)',
-      'sepia(1)'
-    ]
-
     // 1,  3,  4,  5,  0,  2
     // px, nx, py, ny, pz, nz
     this.panorama = new PANOLENS.CubePanorama(cube)
     this.viewer = new PANOLENS.Viewer(options)
     this.viewer.disableControl()
-
-    this.panorama.addEventListener('progress', this.onProgress.bind(this))
-    this.panorama.addEventListener('enter', this.onEnter.bind(this))
-    this.panorama.addEventListener('click', this.onClick.bind(this))
-    this.panorama.addEventListener('load', () => document.body.style.filter = 'none')
   }
 
   start() {
     this.viewer.add(this.panorama)
   }
 
-  destroy() {
-    setTimeout(() => {
-      this.progress.remove()
-      this.panorama.dispose()
-      this.viewer.remove(this.panorama)
-      this.panorama.container.remove()
-      this.panorama = null
-    }, 1000)
-  }
-
-  renderProgress() {
-    this.progress = document.createElement('div')
-    this.progress.classList.add('progress')
-    document.body.appendChild(this.progress)
-  }
-
-  onEnter(event) {
-    this.progress.style.width = 0
-    this.progress.classList.remove('finish')
-  }
-
-  onClick(event) {
-    if (event.mouseEvent.which === 2) {
-      document.body.style.filter = this.filters[this.randomInt(0, this.filters.length - 1)]
-    }
-  }
-
-  onProgress(event) {
-    const progress = event.progress.loaded / event.progress.total * 100
-    this.progress.style.width = progress + '%'
-
-    if (progress === 100) {
-      this.progress.classList.add('finish')
-    }
-  }
-
-  randomInt(min, max) {
-    return Math.floor(min + Math.random() * (max + 1 - min))
+  updateTextures(textures) {
+    this.panorama.material.uniforms.tCube.value = textures
   }
 }
