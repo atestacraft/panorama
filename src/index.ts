eeb7c2586d32a66df6dc55353cbb9929a2a0c6a9
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-07-23 22:50:22 +0800

    feat: add 1.19, 1.20 panorama

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
-import {
-  BackSide,
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  Scene,
-  TextureLoader,
-  WebGLRenderer
-} from 'three'
+import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-const scene = new Scene()
-const version = '1.15'
+const versions = [
+  '1.13',
+  '1.14',
+  '1.15',
+  '1.16',
+  '1.17',
+  '1.18',
+  '1.19',
+  '1.20'
+]
+
+const searchParams = new URLSearchParams(location.search)
+const version = searchParams.get('version') ?? '1.17'
+
+if (!versions.includes(version)) {
+  location.href = '/'
+}
+
+const url = new URL(location.href)
+url.searchParams.set('version', version)
+history.pushState(null, '', url)
+
+const filePath = `images/${version}`
+document.body.style.backgroundImage = `url(${filePath}/background.png)`
+
 const images = [
   'panorama_1',
   'panorama_3',
@@ -19,30 +33,25 @@ const images = [
   'panorama_5',
   'panorama_0',
   'panorama_2'
-].map((fileName) => `images/${version}/${fileName}.png`)
+].map((fileName) => `${filePath}/${fileName}.png`)
 
-const cubeGeometry = new BoxGeometry(1000, 1000, 1000)
-const textureLoader = new TextureLoader()
-const cubeMaterials = images.map((image) => {
-  return new MeshBasicMaterial({
-    map: textureLoader.load(image),
-    side: BackSide
-  })
-})
-const cube = new Mesh(cubeGeometry, cubeMaterials)
-scene.add(cube)
-
-const camera = new PerspectiveCamera(
-  85,
+const camera = new THREE.PerspectiveCamera(
+  100,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
-camera.position.set(-1, 0, -1)
+camera.position.set(0.2, 0.2, -1)
+
+const scene = new THREE.Scene()
 scene.add(camera)
 
-const renderer = new WebGLRenderer()
+const textureCube = await new THREE.CubeTextureLoader().loadAsync(images)
+scene.background = textureCube
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 
 const container = document.querySelector('.container')!
 container.appendChild(renderer.domElement)
@@ -51,10 +60,10 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableRotate = false
 controls.enableZoom = false
 
-function animate(): void {
-  requestAnimationFrame(animate)
-  camera.rotation.y += 0.0003
+function render(): void {
+  requestAnimationFrame(render)
+  camera.rotation.y += 0.0004
   renderer.render(scene, camera)
 }
 
-animate()
+render()
